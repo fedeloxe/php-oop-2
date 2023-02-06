b3e55902ea624f69b783c1c623d535f0545ea9a4
fedeloxe <loxeee@gmail.com>
2023-02-06 17:19:35 +0100

    food done

--- database.php
+++ database.php
@@ -3,8 +3,8 @@ include __DIR__ . '/Models/products.php';
 include __DIR__ . '/Models/accesories.php';
 include __DIR__ . '/Models/food.php';
 
-$food1 = new food("Royal Canin Mini Adult", "43,99", "https://m.media-amazon.com/images/I/71fwkZg9m6L._AC_SY679_.jpg", "545g", "cane", "prosciutto , riso");
-$food2 = new food("Almo Nature Holistic Maintennance Medium Large Tonno e Riso", "44,99", "https://www.bauzaar.it/media/catalog/product/t/o/tonno_e_riso_cane_1.jpg?width=700&height=700&store=default&image-type=image", "600g", "cane", "manzo,cereali");
-$food3 = new food("Almo nature Cat Daily Lattina", "34,99", "https://www.amoreanimaleshop.it/wp-content/uploads/2021/02/Almo-Nature-Cat-Daily-Coniglio-400.jpg", "400g", "gatto", "tonno,pollo,prosciutto");
-$food4 = new food("Mangime per Pesci Guppy in Fiocchi", "2,95", "https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg?v=1762045775", "30g", "pesce", "Pesci e sottoprodotti dei pesci, cereali,lieviti,alghe");
+$food1 = new food("Royal Canin Mini Adult", "43,99", "https://m.media-amazon.com/images/I/71fwkZg9m6L._AC_SY679_.jpg", "545g", '<i class="fa-solid fa-dog"></i> cane', "prosciutto , riso");
+$food2 = new food("Almo Nature Holistic Maintennance Medium Large Tonno e Riso", "44,99", "https://www.bauzaar.it/media/catalog/product/t/o/tonno_e_riso_cane_1.jpg?width=700&height=700&store=default&image-type=image", "600g", '<i class="fa-solid fa-dog"></i> cane', "manzo,cereali");
+$food3 = new food("Almo nature Cat Daily Lattina", "34,99", "https://www.amoreanimaleshop.it/wp-content/uploads/2021/02/Almo-Nature-Cat-Daily-Coniglio-400.jpg", "400g", '<i class="fa-solid fa-cat"></i> gatto ', "tonno,pollo,prosciutto");
+$food4 = new food("Mangime per Pesci Guppy in Fiocchi", "2,95", "https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg?v=1762045775", "30g",  '<i class="fa-solid fa-fish"></i> pesce', "Pesci e sottoprodotti dei pesci, cereali,lieviti,alghe");
 $arrayFoods = [$food1, $food2, $food3, $food4];
--- index.php
+++ index.php
@@ -10,20 +10,23 @@ include __DIR__ . '/database.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>PHP-OOP-2</title>
 </head>
 
 <body>
     <div class="container pt-5">
         <h1> I nostri prodotti :</h1>
-        <div class="  pt-5 row  justify-content-around">
+        <div class="  pt-5 row  justify-content-around ">
             <?php foreach ($arrayFoods as $child) { ?>
-                <div class="card col-4 object-fit">
-                    <img class="h-50 object-fit-contain" src="<?php echo $child->img; ?>" alt="prodotto">
-
-
+                <div class="card col-3 m-5 object-fit">
+                    <img class="h-50 p-1 object-fit-contain" src="<?php echo $child->img; ?>" alt="prodotto">
+                    <h2><?php echo  $child->title; ?> </h2>
+                    <p><?php echo  $child->race; ?> </p>
+                    <p>Prezzo: &#8364; <?php echo  $child->price; ?></p>
+                    <p>Peso netto: <?php echo  $child->weight; ?></p>
+                    <p>Ingredienti: <?php echo  $child->ingredients; ?></p>
                 </div>
-
             <?php } ?>
 
         </div>
